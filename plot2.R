17dbe164f32e628fb85cf19e49dd06e90385499f
cyurys <pyrus@tut.by>
2014-10-12 22:27:08 +0300

    plot2 files

---
+++ plot2.R
@@ -0,0 +1,29 @@
+# Exploratory Data Analysis (Coursera)
+# Course Project 1
+# plot2.R file
+
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+fileZip <- "household_power_consumption.zip"
+file <- "household_power_consumption.txt"
+dates <- c(as.Date("2007-02-01"), as.Date("2007-02-02"))
+
+## download and unzip file if needed
+if (!file.exists(file)) {
+	download.file(fileUrl, destfile=fileZip)
+	unzip(fileZip)
+}
+## load (read.table works slow)
+data <- read.table(file, header=TRUE, sep=";", stringsAsFactors=FALSE)
+
+## subset to dates
+data <- data[which(as.Date(data$Date, "%d/%m/%Y") %in% dates), ]
+data$datetime <- strptime(paste(data$Date, data$Time), "%d/%m/%Y %H:%M:%S")
+
+# Unnecessary sring, but otherwise you get weekdays in your language (which may be not "en_US")
+Sys.setlocale("LC_TIME", "en_US")
+
+## plot PNG 480x480
+png(filename="plot2.png", width=480, height=480)
+plot(x=data$datetime, y=data$Global_active_power, type="l", xlab="", ylab="Global Active Power (kilowatts)")
+dev.off()
+
